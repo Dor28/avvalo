c52fbfd89ff7c48a6c11abfca271014b5f028240
Eldor Pulatov <psanti99.sp@gmail.com>
2026-07-04 01:11:35 +0500

    Shorten feedback migration revision id

--- alembic/versions/0002_feedback_nullable.py
+++ alembic/versions/0002_feedback_nullable.py
@@ -1,6 +1,6 @@
 """allow usefulness feedback before next action
 
-Revision ID: 0002_feedback_next_action_nullable
+Revision ID: 0002_feedback_nullable
 Revises: 0001_initial
 Create Date: 2026-06-28
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 from alembic import op
 
-revision: str = "0002_feedback_next_action_nullable"
+revision: str = "0002_feedback_nullable"
 down_revision: str | None = "0001_initial"
 branch_labels: Sequence[str] | None = None
 depends_on: Sequence[str] | None = None
